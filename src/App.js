d82249d6c649410bea131c755c5e7887878dc71f
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2019-10-23 11:40:10 -0500

    skeleton loader

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { SkeletonLine, SkeletonCircle } from "./components/Skeleton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  });
+
+  let card1;
+  if (loading) {
+    card1 = (
+      <div className="card1">
+        <header className="card1__header">
+          <div className="card1__profile-wrapper">
+            <SkeletonCircle size={60} translucent={true} />
+          </div>
+          <SkeletonLine width={30} translucent={true} />
+        </header>
+        <p className="card1__text">
+          <SkeletonLine translucent={true} />
+          <SkeletonLine width={50} translucent={true} />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      </div>
+    );
+  } else {
+    card1 = (
+      <div className="card1">
+        <header className="card1__header">
+          <div className="card1__profile-wrapper">
+            <img
+              className="card1__profile-img"
+              src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt="profile image"
+            />
+          </div>
+          <h3 className="card1__profile-name">John Doe</h3>
+        </header>
+        <p className="card1__text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In finibus
+          nec mi ut suscipit. Fusce blandit nec mauris vitae posuere.
+        </p>
+      </div>
+    );
+  }
+  return card1;
+};
 
 export default App;
---
+++ src/components/Skeleton/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+
+const SSkeletonPulse = styled.div`
+  display: inline-block;
+  height: 100%;
+  width: 100%;
+  //   background: linear-gradient(-90deg, #f0f0f0 0%, #f8f8f8 50%, #f0f0f0 100%);
+  background: ${props =>
+    props.translucent
+      ? `linear-gradient(-90deg, #c1c1c1 0%, #f8f8f8 50%, #c1c1c1 100%)`
+      : `linear-gradient(-90deg, #f0f0f0 0%, #f8f8f8 50%, #f0f0f0 100%)`};
+  background-size: 400% 400%;
+  animation: pulse 1.2s ease-in-out infinite;
+  @keyframes pulse {
+    0% {
+      background-position: 0% 0%;
+    }
+    100% {
+      background-position: -135% 0%;
+    }
+  }
+`;
+
+const SSkeletonLine = styled(SSkeletonPulse)`
+  width: ${props => (props.width ? props.width + "%" : "100%")};
+  height: ${props => (props.height ? props.height + "px" : "14px")};
+  border-radius: 2px;
+  float: ${props => props.float && props.float};
+
+  &::before {
+    content: "\00a0";
+  }
+`;
+
+const SSkeletonCircle = styled(SSkeletonPulse)`
+  border-radius: 50%;
+  width: ${props => (props.size ? props.size + "px" : "50px")};
+  height: ${props => (props.size ? props.size + "px" : "50px")};
+  float: ${props => props.float && props.float};
+
+  &::before {
+    content: "\00a0";
+  }
+`;
+
+export const SkeletonLine = props => <SSkeletonLine {...props} />;
+export const SkeletonCircle = props => <SSkeletonCircle {...props} />;
