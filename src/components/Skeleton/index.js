61633cba7fa02f13f3c0bc6fa1b01af54d9fd19c
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2019-10-24 12:16:29 -0500

    removed height and width from pulse animation

--- src/components/Skeleton/index.js
+++ src/components/Skeleton/index.js
@@ -3,8 +3,6 @@ import styled from "styled-components";
 
 const SSkeletonPulse = styled.div`
   display: inline-block;
-  height: 100%;
-  width: 100%;
   //   background: linear-gradient(-90deg, #f0f0f0 0%, #f8f8f8 50%, #f0f0f0 100%);
   background: ${props =>
     props.translucent
